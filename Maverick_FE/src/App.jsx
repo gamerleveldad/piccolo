fc46aceab4bfb039c7201b1cf440d9c7494171c6
Trey Patterson <treyvpatterson@gmail.com>
2026-07-28 00:44:08 -0400

    Moving files around

--- Maverick_FE/src/App.jsx
+++ Maverick_FE/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Activity, CloudRain, Gamepad2, Tv, Map as MapIcon } from 'lucide-react';
-import MapWidget from './components/ui/MapWidget';
+import MapWidget from './components/MapWidget';
 
 function App() {
   // Data state variables
