acda05f1b64ccb6f111fd98b82d457005e307a44
Trey Patterson <treyvpatterson@gmail.com>
2026-07-28 01:00:17 -0400

    Pushing the fix

--- Maverick_FE/src/components/MapWidget.jsx
+++ Maverick_FE/src/components/MapWidget.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import maplibregl from 'maplibre-gl';
+import * as maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 // Read coordinates from environment variables with fallbacks
