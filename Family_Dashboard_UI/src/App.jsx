682671672d6db90ed3372ddce23fd54f927a9e82
Trey Patterson <treyvpatterson@gmail.com>
2026-08-03 16:16:19 -0400

    minor integration changes

--- Family_Dashboard_UI/src/App.jsx
+++ Family_Dashboard_UI/src/App.jsx
@@ -29,13 +29,40 @@ function App() {
           console.error("Backend error payload:", data.error);
           return; 
         }
+
+        // --- SCHEMA MAPPING: Bridge the new weather_api keys to legacy widget keys ---
+        if (data.weather) {
+           data.weather.temperature_f = data.weather.temp_f;
+           data.weather.feels_like_f = data.weather.feels_like_f;
+           data.weather.pressure_inhg = data.weather.sea_level_pressure_inhg;
+           data.weather.icon_api = data.weather.icon;
+           data.weather.rain_accumulation_day_in = data.weather.precip_in;
+           data.weather.humidity_pct = data.weather.relative_humidity;
+           data.weather.wind_speed_mph = data.weather.wind_avg_mph;
+           data.weather.wind_gust_mph = data.weather.wind_gust_mph;
+        }
+
+        if (data.forecast_daily && data.forecast_daily.length > 0) {
+           // Populate the current rain chance from today's daily forecast
+           if (data.weather) {
+              data.weather.rain_chance_current = data.forecast_daily[0].precip_probability;
+           }
+
+           // Map the daily forecast array
+           data.forecast_daily = data.forecast_daily.map(d => ({
+              ...d,
+              high: d.temp_max_f,
+              low: d.temp_min_f,
+              rain_pct: d.precip_probability
+           }));
+        }
+
         setDashboardState(data);
       }
     } catch (err) {
       console.error("Failed fetching dashboard state:", err);
     }
   };
-
   useEffect(() => {
     fetchState();
     const interval = setInterval(fetchState, 30000);
@@ -88,7 +115,6 @@ function App() {
           </div>
 
           <div className="grid grid-cols-2 gap-3 mt-1 h-full min-h-[160px]">
-            <AnimeWidget shows={dashboardState.anime_progress} />
             <GooglePhotosWidget apiBase={apiBase} />
           </div>
         </div>
